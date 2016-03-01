97c12daa94f0cb0bd754c103aae22b975d53a2c0
Vlad Niculescu <vlg.niculescu@gmail.com>
2016-03-01 12:46:15 +0000

    GridLayout + center text

--- src/alexvlad/view/FormationPanel.java
+++ src/alexvlad/view/FormationPanel.java
@@ -9,11 +9,14 @@ public class FormationPanel extends JPanel {
     public FormationPanel(int def, int mid, int at) {
 
         super();
-
+        setLayout(new GridLayout(5,1,20,20));
+        setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
         setMinimumSize(new Dimension(300, 300));
+        add(addLine(1,"GoalKeeper"));
         add(addLine(def,"Defender"));
         add(addLine(mid,"Midfielder"));
         add(addLine(at,"Striker"));
+        add(addLine(4,"Sub"));
 
 
         
@@ -22,7 +25,8 @@ public class FormationPanel extends JPanel {
     private JPanel addLine(int number, String name)
     {
         JPanel line = new JPanel();
-        line.setLayout(new GridLayout(1,number));
+        line.setLayout(new GridLayout(1,number,20,20));
+        setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
         for(int i=0;i<number;i++)
         {
             line.add(createPlayerPanel(name));
@@ -34,9 +38,10 @@ public class FormationPanel extends JPanel {
     {
         JPanel player = new JPanel();
         player.setLayout(new BorderLayout());
-        JButton playerButton = new JButton();
-        JTextField playerName = new JTextField();
-        playerName.setText(name);
+        JButton playerButton = new JButton("+");
+        JTextField playerName = new JTextField(name);
+
+   playerName.setHorizontalAlignment(JLabel.CENTER);
 
         player.add(playerButton,BorderLayout.CENTER);
         player.add(playerName,BorderLayout.SOUTH);
